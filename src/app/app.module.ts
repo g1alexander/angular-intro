8f9d47b6ab7b994f531883ee6a487aea8ab5f2b1
g1alexander <alexlds26@gmail.com>
2021-07-19 10:31:14 -0500

    bases - parte 2 antes de los servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CountModule } from './counts/count.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CountModule, HeroesModule],
+  imports: [BrowserModule, CountModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
